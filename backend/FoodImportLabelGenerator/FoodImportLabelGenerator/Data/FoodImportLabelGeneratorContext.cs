455eaca7d70a623accea107891483e94c5928f1c
mayu0126 <ildikopinter0126@gmail.com>
2023-10-18 00:24:49 +0200

    Add initial test data to the database

--- backend/FoodImportLabelGenerator/FoodImportLabelGenerator/Data/FoodImportLabelGeneratorContext.cs
+++ backend/FoodImportLabelGenerator/FoodImportLabelGenerator/Data/FoodImportLabelGeneratorContext.cs
@@ -6,15 +6,18 @@ public class FoodImportLabelGeneratorContext : DbContext
 {
     public DbSet<Label> Labels { get; set; }
     private readonly IConfiguration _configuration;
+    private readonly string _configString;
 
-    public FoodImportLabelGeneratorContext(IConfiguration configuration)
+    public FoodImportLabelGeneratorContext(IConfiguration configuration, string configString)
     {
         _configuration = configuration;
+        _configString = configString;
     }
     
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        var connectionString = _configuration.GetConnectionString("DefaultConnection");
+        var connectionString = _configString;
+        //var connectionString = _configuration.GetConnectionString("ConnectionStrings:DefaultConnection");
         optionsBuilder.UseSqlServer(connectionString);
     }
 }
--- backend/FoodImportLabelGenerator/FoodImportLabelGenerator/Program.cs
+++ backend/FoodImportLabelGenerator/FoodImportLabelGenerator/Program.cs
@@ -1,3 +1,6 @@
+using FoodImportLabelGenerator;
+using FoodImportLabelGenerator.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -31,5 +34,57 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers(); // Adds endpoints for controller actions without specifying any routes
+
+var configString = builder.Configuration["ConnectionStrings:DefaultConnection"];
+
+void InitializeDb()
+{
+    using var db = new FoodImportLabelGeneratorContext(new ConfigurationManager(), configString);
+    InitializeLabels();
+    PrintLabels();
+
+    void InitializeLabels()
+    {
+        db.Add(new Label
+        {
+            //Id = 1,
+            Date = DateTime.Now,
+            LegalName = "Chocolate",
+            Nutritions = "calories",
+            Producer = "Mayu Kft.",
+            Distributor = "Mayu Kft.",
+            CountryOfOrigin = "Hungary",
+            NetWeight = 500,
+            Storage = "20 °C",
+            BBD = new DateTime(2023, 12,12),
+            Organic = false
+        });
+        db.Add(new Label()
+        {
+            //Id = 2,
+            Date = DateTime.Now,
+            LegalName = "Candy",
+            Nutritions = "calories",
+            Producer = "Mayu Kft.",
+            Distributor = "Mayu Kft.",
+            CountryOfOrigin = "Hungary",
+            NetWeight = 300,
+            Storage = "25 °C",
+            BBD = new DateTime(2023, 10, 30),
+            Organic = true
+        });
+        db.SaveChanges(); // required to save changes
+    }
+
+    void PrintLabels() // reads all the data from the Labels DbSet
+    {
+        foreach (var label in db.Labels)
+        {
+            Console.WriteLine(label.LegalName);
+        }
+    }
+}
+
+InitializeDb();
 
 app.Run(); // Starts the web server
