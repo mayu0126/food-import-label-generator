57075d586544cda98f08bfb4c9070da1deb2e22d
mayu0126 <ildikopinter0126@gmail.com>
2023-10-19 19:35:58 +0200

    Add AuthController

--- backend/FoodImportLabelGenerator/FoodImportLabelGenerator/Controllers/AuthController.cs
+++ backend/FoodImportLabelGenerator/FoodImportLabelGenerator/Controllers/AuthController.cs
@@ -1,6 +1,44 @@
+using FoodImportLabelGenerator.Contracts;
+using FoodImportLabelGenerator.Services.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
 namespace FoodImportLabelGenerator.Controllers;
 
-public class AuthController
+[ApiController]
+[Route("[controller]")]
+public class AuthController : ControllerBase
 {
-    
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("Register")]
+    public async Task<ActionResult<RegistrationResponse>> Register(RegistrationRequest request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var result = await _authService.RegisterAsync(request.Email, request.Username, request.Password);
+
+        if (!result.Success)
+        {
+            AddErrors(result);
+            return BadRequest(ModelState);
+        }
+
+        return CreatedAtAction(nameof(Register), new RegistrationResponse(result.Email, result.UserName));
+    }
+
+    private void AddErrors(AuthResult result)
+    {
+        foreach (var error in result.ErrorMessages)
+        {
+            ModelState.AddModelError(error.Key, error.Value);
+        }
+    }
 }
