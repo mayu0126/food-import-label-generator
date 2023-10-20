52b12fec81e6267a691721b54c73150c74e270c5
mayu0126 <ildikopinter0126@gmail.com>
2023-10-20 18:10:01 +0200

    Update Program.cs with create admin if not exists

--- backend/FoodImportLabelGenerator/FoodImportLabelGenerator/Program.cs
+++ backend/FoodImportLabelGenerator/FoodImportLabelGenerator/Program.cs
@@ -68,6 +68,7 @@ InitializeDb();
 */
 
 AddRoles();
+AddAdmin();
 
 app.Run(); // Starts the web server
 
@@ -194,4 +195,27 @@ async Task CreateAdminRole(RoleManager<IdentityRole> roleManager)
 async Task CreateUserRole(RoleManager<IdentityRole> roleManager)
 {
     await roleManager.CreateAsync(new IdentityRole("User")); //The role string should better be stored as a constant or a value in appsettings
+}
+
+void AddAdmin()
+{
+    var tAdmin = CreateAdminIfNotExists();
+    tAdmin.Wait();
+}
+
+async Task CreateAdminIfNotExists()
+{
+    using var scope = app.Services.CreateScope();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+    var adminInDb = await userManager.FindByEmailAsync("admin@admin.com");
+    if (adminInDb == null)
+    {
+        var admin = new IdentityUser { UserName = "admin", Email = "admin@admin.com" };
+        var adminCreated = await userManager.CreateAsync(admin, "admin123");
+
+        if (adminCreated.Succeeded)
+        {
+            await userManager.AddToRoleAsync(admin, "Admin");
+        }
+    }
 }
