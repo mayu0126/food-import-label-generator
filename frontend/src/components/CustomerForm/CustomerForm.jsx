54215d4559968bb97105fe5a9dfe96beacd2a0a5
mayu0126 <ildikopinter0126@gmail.com>
2023-10-26 20:30:45 +0200

    Update CustomerForm

--- frontend/src/components/CustomerForm/CustomerForm.jsx
+++ frontend/src/components/CustomerForm/CustomerForm.jsx
@@ -17,46 +17,67 @@ const CustomerForm = ({ onCancel, onSave, disabled, isRegister, errorMessage }) 
     };
 
     return (
-    <>
-    <form className="CustomerForm" onSubmit={onSubmit}>
-
-        {isRegister&&<div className="control">
-        <label htmlFor="userName">Username:</label>
-        <input
-            name="userName"
-            id="userName"
-        />
-        </div>}
-
-        <div className="control">
-        <label htmlFor="email">Email:</label>
-        <input
-            name="email"
-            id="email"
-        />
-        </div>
-
-        <div className="control">
-        <label htmlFor="password">Password:</label>
-        <input
-            type="password"
-            name="password"
-            id="password"
-        />
-        </div>
-
-        <div className="buttons">
-        <button type="submit" disabled={disabled}>
-            {isRegister? "Register" : "Log In"}
-        </button>
-
-        <button type="button" onClick={onCancel}>
-            Cancel
-        </button>
-        </div>
-    </form>
-    {errorMessage && <p className="error-message">{errorMessage}</p>}
-    </>
+        <div className="mx-auto max-w-xs py-10 sm:py-16 lg:py-20"> {/* Adjust padding */}
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={onSubmit}>
+          {isRegister && (
+            <div className="mb-4"> {/* Add margin bottom */}
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="userName">
+                Username:
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                name="userName"
+                id="userName"
+                type="text"
+              />
+            </div>
+          )}
+  
+          <div className="mb-4"> {/* Add margin bottom */}
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+              Email:
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              name="email"
+              id="email"
+              type="email"
+            />
+          </div>
+  
+          <div className="mb-4"> {/* Add margin bottom */}
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+              Password:
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="password"
+              name="password"
+              id="password"
+            />
+          </div>
+  
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-rose-600 hover:bg-rose-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+              disabled={disabled}
+            >
+              {isRegister ? "Register" : "Log In"}
+            </button>
+            <button
+              className="bg-slate-500 hover:bg-slate-400 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+              onClick={onCancel}
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+        {errorMessage && (
+          <p className="text-red-500 text-xs italic">{errorMessage}</p>
+        )}
+      </div>
     );
 };
 
