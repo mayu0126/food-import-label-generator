af2ee145b3a45b1067918bcac7dfd6896c4f8694
mayu0126 <ildikopinter0126@gmail.com>
2023-11-02 00:44:03 +0100

    Add UserLabels page and LabelTable component

---
+++ frontend/src/components/LabelTable/LabelTable.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; // Importáld be a Link komponenst
+import PropTypes from 'prop-types';
+
+const LabelTable = ({ labelData }) => {
+  return (
+    <div className="mx-auto mt-20 max-w-screen-xl py-10 sm:py-16 lg:py-20">
+      <table className="bg-white shadow-md rounded w-full">
+        <thead>
+          <tr>
+            <th className="text-left px-6 py-3">Date</th>
+            <th className="text-left px-6 py-3">ProductName</th>
+            <th className="text-left px-6 py-3">LegalName</th>
+            <th className="text-left px-6 py-3">Producer</th>
+            <th className="text-left px-6 py-3">Distributor</th>
+            <th className="text-left px-6 py-3">EAN</th>
+            <th className="text-left px-6 py-3"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {labelData && labelData.map((item, index) => (
+            <tr key={index}>
+              <td className="px-6 py-4">{item.date.substring(0, 10)}</td>
+              <td className="px-6 py-4">{item.productName}</td>
+              <td className="px-6 py-4">{item.legalName}</td>
+              <td className="px-6 py-4">{item.producer}</td>
+              <td className="px-6 py-4">{item.distributor}</td>
+              <td className="px-6 py-4">{item.ean}</td>
+              <td className="px-6 py-4">
+                <Link
+                  className="bg-rose-600 hover:bg-rose-500 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline"
+                  to={`/details/${item.id}`}
+                >
+                  Details
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+LabelTable.propTypes = {
+  labelData: PropTypes.arrayOf(PropTypes.shape({
+    date: PropTypes.string,
+    productName: PropTypes.string,
+    legalName: PropTypes.string,
+    producer: PropTypes.string,
+    distributor: PropTypes.string,
+    ean: PropTypes.string,
+    id: PropTypes.number,
+  })).isRequired,
+};
+
+export default LabelTable;
--- frontend/src/pages/UserLabels/UserLabels.jsx
+++ frontend/src/pages/UserLabels/UserLabels.jsx
@@ -1,7 +1,8 @@
 import { React, useState, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../index";
-import ProfileData from "../../components/ProfileData/ProfileData";
+
+import LabelTable from "../../components/LabelTable/LabelTable";
 
 //This page will GET the user's labels and use the LabelTable component to display them
 //If the user clicks on the label's name, itt will shows a Label component on a new page
@@ -43,18 +44,20 @@ const saveUserData = (user, userName, token) => {
 */
 function UserLabels() {
 
+    const context = useContext(UserContext); //connect to UserContext - email, userName, token
+    const [currentUser, setCurrentUser] = useState(""); //save actual user
+
+    const [labelData, setLabelData] = useState([]);
+
     /*
     const [errorMessage, setErrorMessage] = useState("");
     const [isEdit, setIsEdit] = useState(false);
     const [loading, setLoading] = useState(false);
     const [isDisabled, setIsDisabled] = useState(true);
-
-    const [currentUser, setCurrentUser] = useState(""); //save actual user
-    const context = useContext(UserContext); //connect to UserContext - email, userName, token
     const navigate = useNavigate();
-
+*/
     useEffect(() => {
-        console.log("belépett a useEffectbe")
+        console.log("GET profile data")
         fetch(`${url}/User/GetByUserNameAsync/${context.user.userName}`, {
             method: "GET",
             headers: {
@@ -62,8 +65,21 @@ function UserLabels() {
             "Authorization": "Bearer " + context.user.token
             },
         }).then((res) => res.json()).then((data) => setCurrentUser(data))
-    }, []);    
+    }, []);  
 
+    useEffect(() => {
+        console.log("GET labels data")
+        console.log(currentUser.id)
+        fetch(`${url}/Label/GetByUserIdAsync/4c4dae67-93d6-4550-8ad5-bc79e2a4c082`, {
+            method: "GET",
+            headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + context.user.token
+            },
+        }).then((res) => res.json()).then((data) => setLabelData(data))
+    }, [currentUser]);    
+
+    /*
     const handleSaveProfileData = (user, userName) => {
         setLoading(true);
         console.log(context.user); //who signed in
@@ -84,7 +100,11 @@ function UserLabels() {
           });
       };
 */
-    return (null
+      console.log(labelData);
+    return (
+        <LabelTable
+            labelData={labelData}
+        />
         /*
     <ProfileData
         isEdit={isEdit}
