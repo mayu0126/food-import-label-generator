0f8150cee8e519277b23bb6a48919742fe079348
mayu0126 <ildikopinter0126@gmail.com>
2023-11-05 00:51:36 +0100

    Add Google Cloud Translation Basic

---
+++ backend/FoodImportLabelGenerator/FoodImportLabelGenerator/Services/Translation/GoogleTranslationService.cs
@@ -0,0 +1,27 @@
+using FoodImportLabelGenerator.Contracts;
+using Google.Cloud.Translation.V2;
+
+namespace FoodImportLabelGenerator.Services.Translation;
+
+public class GoogleTranslationService
+{
+    private TranslationClient _translationClient;
+
+    public GoogleTranslationService(string apiKey)
+    {
+        var clientBuilder = TranslationClient.CreateFromApiKey(apiKey);
+        _translationClient = clientBuilder;
+    }
+
+    public async Task<string> TranslateText(string text, string targetLanguage, string sourceLanguage)
+    {
+        var response = _translationClient.TranslateText(text, targetLanguage, sourceLanguage);
+
+        if (response.TranslatedText.Length > 0)
+        {
+            return response.TranslatedText;
+        }
+
+        throw new Exception("Translation failed.");
+    }
+}
