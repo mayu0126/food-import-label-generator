4ac0127d598bf339fc5c561b4a1f682d69c45080
mayu0126 <ildikopinter0126@gmail.com>
2023-11-15 12:20:37 +0100

    Add pdf-lib and handle 'ő' and 'ű' characters

--- frontend/src/utils/pdfGenerator.js
+++ frontend/src/utils/pdfGenerator.js
@@ -1,157 +1,131 @@
-import { jsPDF } from 'jspdf';
-import euroleaf from '../assets/images/eu-organic-logo.jpg';
-    
-    // Generate pdf
-    const generatePDF = async (formFields) => {
-        console.log("generatePDF");
+import { PDFDocument, rgb, StandardFonts } from 'pdf-lib';
+//import axios from 'axios';
+import { saveAs } from 'file-saver';
 
-        // Create a new jsPDF instance
-        const doc = new jsPDF({
-            orientation: 'portrait', //portrait
-            unit: 'mm',
-            format: 'a8',
-            //a7 (74.25 mm * 105 mm), a8 (52.5 mm * 74.25 mm) - x-height min. 0.9 mm - font size: min. 6 pt
-            //a6 (105 mm * 148.5 mm) - x-height min 1.2 mm - font size: min. 8 pt (times), min. 7 pt (arial, helvetice, univers)
-        });
-        
-        const addOrganicLogo = (doc) => {
-            return doc.addImage(euroleaf, "JPEG", 50, 100, 24, 16)
-            
-            //return ""; //+organic text??
-        }
-        //addOrganicLogo(doc);
-        
-      
-        // Calculate the dimensions for each quadrant
-        
-        const pageWidth = doc.internal.pageSize.getWidth();
-        const pageHeight = doc.internal.pageSize.getHeight();
+const LiberationSerifRegular = '...';  // Add the actual path to LiberationSerif-Regular.ttf
+const euroleaf = '...';  // Add the actual path to the image file
+
+const generatePDF = async (formFields) => {
+  console.log('generatePDF');
+
+  const pdfDoc = await PDFDocument.create();
+  const page = pdfDoc.addPage();
+
+  // Use a Unicode font
+  const font = await pdfDoc.embedFont(StandardFonts.TimesRoman);
+  page.setFont(font);
+
+  const { width, height } = page.getSize();
+
+  /*const addOrganicLogo = async () => {
+    const imagePath = 'https://www.fibl.org/fileadmin/news_images/eu-logo.jpg';
+    const response = await axios.get(imagePath, { responseType: 'arraybuffer' });
+    const image = await pdfDoc.embedJpg(response.data);
+    page.drawImage(image, { x: 50, y: height - 100, width: 24, height: 16 });
+  };
+
+  await addOrganicLogo();
+
+  */
+  function replaceSpecialCharacters(str) {
+
+    if (typeof str !== 'string') {
+      return str; //if not string, return the original value
+    }
+
+    return str.replace(/[űŰőŐ]/g, function(match) {
+      switch (match) {
+        case 'ű': return 'û';
+        case 'Ű': return 'Û';
+        case 'ő': return 'õ';
+        case 'Ő': return 'Õ';
+        default: return match;
+      }
+    });
+  }
+
+  const pdfFormFields = {
+    productName: formFields.productName,
+    legalName: formFields.legalName,
+    allergens: formFields.allergens,
+    legalNameAdditionalInformation: formFields.legalNameAdditionalInformation,
+    cookingInstructions: formFields.cookingInstructions,
+    ingredientsList: formFields.ingredientsList,
+    ingredientsListAdditionalInformation: formFields.ingredientsListAdditionalInformation,
+    mayContain: formFields.mayContain,
+    nutritions: formFields.nutritions,
+    producer: formFields.producer,
+    distributor: formFields.distributor,
+    countryOfOrigin: formFields.countryOfOrigin,
+    mainIngredientCOO: formFields.mainIngredientCOO,
+    bestBeforeText: formFields.bestBeforeText,
+    storage: formFields.storage,
+    bestBeforeAdditionalInformation: formFields.bestBeforeAdditionalInformation,
+    netWeight: formFields.netWeight,
+    netVolume: formFields.netVolume,
+    organic: formFields.organic,
+    healthMark: formFields.healthMark,
+    ean: formFields.ean,
+  };
+
+  for (const key in formFields) {
+    if (formFields.hasOwnProperty(key)) {
+      pdfFormFields[key] = replaceSpecialCharacters(formFields[key]);
+    }
+  }
+
+    //const LiberationSerifRegularBytes = await axios.get(LiberationSerifRegular, { responseType: 'arraybuffer' }).then((res) => res.data);
+    //const euroleafBytes = await axios.get(euroleaf, { responseType: 'arraybuffer' }).then((res) => res.data);
+  
+    //const font = await pdfDoc.embedFont(LiberationSerifRegularBytes);
+  
+    //page.setFont(font);
+  
+    page.drawText(`LABEL DETAILS - ${formFields.productName}`, { x: 10, y: height - 30, fontSize: 6 });
+  
+    let yOffset = height - 50;
+  
+    for (let field in pdfFormFields) {
+      if (pdfFormFields[field]) {
+        console.log(field);
+        let field_2 = '';
+  
+        const x = 10;
+        const y = yOffset;
+  
+        if (field === 'cookingInstructions') field_2 = 'Elkészítési javaslat: ';
+        if (field === "ingredientsList") field_2 = "Összetevök: "
+        if (field === "nutritions") field_2 = "Átlagos tápérték 100 g termékben: "
+        if (field === "distributor") field_2 = "Forgalmazza: "
+        if (field === "producer") field_2 = "Gyártja: "
+        if (field === "countryOfOrigin") field_2 = "Származási hely: "
+        if (field === "mainIngredientCOO") field_2 = "A fö összetevö származási helye: "
+        if (field === "bestBeforeText") field_2 = "Minöségét megörzi: "
+        if (field === "storage") field_2 = "Tárolás: "
+        if (field === "netWeight") field_2 = "Nettó tömeg: "
+        if (field === "netVolume") field_2 = "Nettó térfogat: "
+
+        // organic logo
         /*
-        const quadrantWidth = pageWidth / 2;
-        const quadrantHeight = pageHeight / 2;
-        */
-
-        // Collect form data
-        const pdfFormFields = {
-            productName: formFields.productName,
-            legalName: formFields.legalName,
-            allergens: formFields.allergens,
-            legalNameAdditionalInformation: formFields.legalNameAdditionalInformation,
-            cookingInstructions: formFields.cookingInstructions,
-            ingredientsList: formFields.ingredientsList,
-            ingredientsListAdditionalInformation: formFields.ingredientsListAdditionalInformation,
-            mayContain: formFields.mayContain,
-            nutritions: formFields.nutritions,
-            producer: formFields.producer,
-            distributor: formFields.distributor,
-            countryOfOrigin: formFields.countryOfOrigin,
-            mainIngredientCOO: formFields.mainIngredientCOO,
-            bestBeforeText: formFields.bestBeforeText,
-            storage: formFields.storage,
-            bestBeforeAdditionalInformation: formFields.bestBeforeAdditionalInformation,
-            netWeight: formFields.netWeight,
-            netVolume: formFields.netVolume,
-            organic: formFields.organic,
-            healthMark: formFields.healthMark,
-            ean: formFields.ean,
-        };
+        if (field === 'organic') {
+            const imagePath = 'https://www.fibl.org/fileadmin/news_images/eu-logo.jpg';
+            const response = await axios.get(imagePath, { responseType: 'arraybuffer' });
+            const image = await pdfDoc.embedJpg(response.data);
+            page.drawImage(image, { x: 40, y: y - 10, width: 10, height: 7 });
     
-        //doc.addFileToVFS('calibri.ttf', binaryString);
-        //doc.addFont('calibri.ttf', 'calibri', 'normal');
-        //doc.setFont('calibri');
-
-        // Start adding content to the PDF
-        doc.setFont('times');
-        //doc.text(`LABEL DETAILS - ${formFields.productName}`, 10, 10);
-
-        doc.setFontSize(8);
-        doc.setLanguage('hu');
-
-        // Loop through the formFields and add them to the PDF
-        let yOffset = 5; //distance from the top
-        for (let field in pdfFormFields) {
-
-            if (pdfFormFields[field]) {
-                console.log(field);
-                let field_2 = "";
-
-                //for (let i = 0; i < 4; i++) {
-                    const x = 3 //distance from the left side //+ (i % 2) * quadrantWidth;
-                    const y = yOffset //+ Math.floor(i / 2) * quadrantHeight;
-
-                    if (field === "cookingInstructions") field_2 = "Elkészítési javaslat: "
-                    if (field === "ingredientsList") field_2 = "Összetevök: "
-                    if (field === "nutritions") field_2 = "Átlagos tápérték 100 g termékben: "
-                    if (field === "distributor") field_2 = "Forgalmazza: "
-                    if (field === "producer") field_2 = "Gyártja: "
-                    if (field === "countryOfOrigin") field_2 = "Származási hely: "
-                    if (field === "mainIngredientCOO") field_2 = "A fö összetevö származási helye: "
-                    if (field === "bestBeforeText") field_2 = "Minöségét megörzi: "
-                    if (field === "storage") field_2 = "Tárolás: "
-                    if (field === "netWeight") field_2 = "Nettó tömeg: "
-                    if (field === "netVolume") field_2 = "Nettó térfogat: "
-
-                    // Check if the field contains "tej" and set font style to bold if true
-                    /*
-                    if (field === "ingredientsList" && pdfFormFields[field].includes("tej")) {
-                        doc.setFont("times", "bold");
-                    } else {
-                        doc.setFont("times", "normal");
-                    }
-                    */
-
-                    // Bold legal name
-                    if (field === "legalName") {
-                        doc.setFont("times", "bold");
-                    }
-                    else {
-                        doc.setFont("times", "normal");
-                    }
-
-                    /*
-                    // Bold storage
-                    if (field === "storage") {
-                        doc.setFont("times", "bold");
-                        doc.text(`${field_2}`, x, y, { maxWidth: pageWidth - 5 });
-                        doc.setFont("times", "normal");
-                        doc.text(`${pdfFormFields[field]}`, x+14, y, { maxWidth: pageWidth - 5 });
-                        continue;
-                    }
-                    */
-
-                    // organic logo
-                    
-                    if (field === "organic") {
-                        const imagePath = 'https://www.fibl.org/fileadmin/news_images/eu-logo.jpg';
-                
-                        // Aszinkron művelet: kép beolvasása
-                        const blob = await fetch(imagePath).then(response => response.blob());
-                
-                        // Aszinkron művelet: kép bázis64 kódolása
-                        const imageData = await new Promise((resolve, reject) => {
-                            const reader = new FileReader();
-                            reader.onloadend = () => resolve(reader.result.split(',')[1]);
-                            reader.onerror = reject;
-                            reader.readAsDataURL(blob);
-                        });
-                
-                        // Kép hozzáadása a PDF-hez
-                        doc.addImage(`data:image/jpeg;base64,${imageData}`, "JPEG", 40, 60, 10, 7);
-                
-                        doc.text(`organic`, 40, 70, { maxWidth: pageWidth - 5 });
-                        yOffset += doc.getTextDimensions(`${field}: ${pdfFormFields[field]}`, { maxWidth: pageWidth - 5 }).h;
-                
-                        continue;
-                    }
-                    
-
-                    doc.text(`${field_2}${pdfFormFields[field]}`, x, y, { maxWidth: pageWidth - 5 });
-                //}
-                yOffset += doc.getTextDimensions(`${field}: ${pdfFormFields[field]}`, { maxWidth: pageWidth - 5 }).h; //spacing can be set by division
-            }
-        }
-        // Save the PDF and open a download dialog
-        doc.save('label.pdf');
-    };
+            page.drawText('organic', { x: 40, y: y, fontSize: 8, color: rgb(0, 0, 0) });
+    
+            yOffset -= 10; // Adjusting yOffset to avoid overlapping
+            continue;
+          }
+        */
+        page.drawText(`${field_2}${pdfFormFields[field]}`, { x: x, y: y, fontSize: 8, color: rgb(0, 0, 0) });
+        yOffset -= 24; // Adjusting yOffset for the next line
+      }
+    }
+            
+    const pdfBytes = await pdfDoc.save();
+    saveAs(new Blob([pdfBytes], { type: 'application/pdf' }), 'label.pdf');
+};
 
     export default generatePDF;
---
+++ frontend/src/utils/pdfGeneratorJSPDF.js
@@ -0,0 +1,158 @@
+import { jsPDF } from 'jspdf';
+import euroleaf from '../assets/images/eu-organic-logo.jpg';
+import LiberationSerifRegular from './LiberationSerif-Regular.ttf'
+    
+    // Generate pdf
+    const generatePDF = async (formFields) => {
+        console.log("generatePDF");
+
+        // Create a new jsPDF instance
+        const doc = new jsPDF({
+            orientation: 'portrait', //portrait
+            unit: 'mm',
+            format: 'a8',
+            //a7 (74.25 mm * 105 mm), a8 (52.5 mm * 74.25 mm) - x-height min. 0.9 mm - font size: min. 6 pt
+            //a6 (105 mm * 148.5 mm) - x-height min 1.2 mm - font size: min. 8 pt (times), min. 7 pt (arial, helvetice, univers)
+        });
+        
+        const addOrganicLogo = (doc) => {
+            return doc.addImage(euroleaf, "JPEG", 50, 100, 24, 16)
+            
+            //return ""; //+organic text??
+        }
+        //addOrganicLogo(doc);
+        
+      
+        // Calculate the dimensions for each quadrant
+        
+        const pageWidth = doc.internal.pageSize.getWidth();
+        const pageHeight = doc.internal.pageSize.getHeight();
+        /*
+        const quadrantWidth = pageWidth / 2;
+        const quadrantHeight = pageHeight / 2;
+        */
+
+        // Collect form data
+        const pdfFormFields = {
+            productName: formFields.productName,
+            legalName: formFields.legalName,
+            allergens: formFields.allergens,
+            legalNameAdditionalInformation: formFields.legalNameAdditionalInformation,
+            cookingInstructions: formFields.cookingInstructions,
+            ingredientsList: formFields.ingredientsList,
+            ingredientsListAdditionalInformation: formFields.ingredientsListAdditionalInformation,
+            mayContain: formFields.mayContain,
+            nutritions: formFields.nutritions,
+            producer: formFields.producer,
+            distributor: formFields.distributor,
+            countryOfOrigin: formFields.countryOfOrigin,
+            mainIngredientCOO: formFields.mainIngredientCOO,
+            bestBeforeText: formFields.bestBeforeText,
+            storage: formFields.storage,
+            bestBeforeAdditionalInformation: formFields.bestBeforeAdditionalInformation,
+            netWeight: formFields.netWeight,
+            netVolume: formFields.netVolume,
+            organic: formFields.organic,
+            healthMark: formFields.healthMark,
+            ean: formFields.ean,
+        };
+    
+        // Add the font directly using addFont
+        //doc.addFont('LiberationSerif.ttf', 'LiberationSerif', 'normal');
+        //doc.setFont('LiberationSerif');
+
+        // Start adding content to the PDF
+        //doc.setFont('times');
+        //doc.text(`LABEL DETAILS - ${formFields.productName}`, 10, 10);
+
+        doc.setFontSize(8);
+        doc.setLanguage('hu');
+
+        // Loop through the formFields and add them to the PDF
+        let yOffset = 5; //distance from the top
+        for (let field in pdfFormFields) {
+
+            if (pdfFormFields[field]) {
+                console.log(field);
+                let field_2 = "";
+
+                //for (let i = 0; i < 4; i++) {
+                    const x = 3 //distance from the left side //+ (i % 2) * quadrantWidth;
+                    const y = yOffset //+ Math.floor(i / 2) * quadrantHeight;
+
+                    if (field === "cookingInstructions") field_2 = "Elkészítési javaslat: "
+                    if (field === "ingredientsList") field_2 = "Összetevök: "
+                    if (field === "nutritions") field_2 = "Átlagos tápérték 100 g termékben: "
+                    if (field === "distributor") field_2 = "Forgalmazza: "
+                    if (field === "producer") field_2 = "Gyártja: "
+                    if (field === "countryOfOrigin") field_2 = "Származási hely: "
+                    if (field === "mainIngredientCOO") field_2 = "A fö összetevö származási helye: "
+                    if (field === "bestBeforeText") field_2 = "Minöségét megörzi: "
+                    if (field === "storage") field_2 = "Tárolás: "
+                    if (field === "netWeight") field_2 = "Nettó tömeg: "
+                    if (field === "netVolume") field_2 = "Nettó térfogat: "
+
+                    // Check if the field contains "tej" and set font style to bold if true
+                    /*
+                    if (field === "ingredientsList" && pdfFormFields[field].includes("tej")) {
+                        doc.setFont("times", "bold");
+                    } else {
+                        doc.setFont("times", "normal");
+                    }
+                    */
+
+                    // Bold legal name
+                    if (field === "legalName") {
+                        doc.setFont("times", "bold");
+                    }
+                    else {
+                        doc.setFont("times", "normal");
+                    }
+
+                    /*
+                    // Bold storage
+                    if (field === "storage") {
+                        doc.setFont("times", "bold");
+                        doc.text(`${field_2}`, x, y, { maxWidth: pageWidth - 5 });
+                        doc.setFont("times", "normal");
+                        doc.text(`${pdfFormFields[field]}`, x+14, y, { maxWidth: pageWidth - 5 });
+                        continue;
+                    }
+                    */
+
+                    // organic logo
+                    
+                    if (field === "organic") {
+                        const imagePath = 'https://www.fibl.org/fileadmin/news_images/eu-logo.jpg';
+                
+                        // Aszinkron művelet: kép beolvasása
+                        const blob = await fetch(imagePath).then(response => response.blob());
+                
+                        // Aszinkron művelet: kép bázis64 kódolása
+                        const imageData = await new Promise((resolve, reject) => {
+                            const reader = new FileReader();
+                            reader.onloadend = () => resolve(reader.result.split(',')[1]);
+                            reader.onerror = reject;
+                            reader.readAsDataURL(blob);
+                        });
+                
+                        // Kép hozzáadása a PDF-hez
+                        doc.addImage(`data:image/jpeg;base64,${imageData}`, "JPEG", 40, 60, 10, 7);
+                
+                        doc.text(`organic`, 40, 70, { maxWidth: pageWidth - 5 });
+                        yOffset += doc.getTextDimensions(`${field}: ${pdfFormFields[field]}`, { maxWidth: pageWidth - 5 }).h;
+                
+                        continue;
+                    }
+                    
+
+                    doc.text(`${field_2}${pdfFormFields[field]}`, x, y, { maxWidth: pageWidth - 5 });
+                //}
+                yOffset += doc.getTextDimensions(`${field}: ${pdfFormFields[field]}`, { maxWidth: pageWidth - 5 }).h; //spacing can be set by division
+            }
+        }
+        // Save the PDF and open a download dialog
+        doc.save('label.pdf');
+    };
+
+    export default generatePDF;
