5a5866f75c90dba84581362c54b63febd96025db
mayu0126 <ildikopinter0126@gmail.com>
2023-11-23 00:52:54 +0100

    Update search possibility in GlossaryTable

--- frontend/src/components/GlossaryTable/GlossaryTable.jsx
+++ frontend/src/components/GlossaryTable/GlossaryTable.jsx
@@ -10,6 +10,31 @@ const GlossaryTable = ({ glossaryData, onDelete, onAdd }) => {
 
   const [showNewWordModal, setShowNewWordModal] = useState(false);
 
+  const [filteredGlossaryData, setFilteredGlossaryData] = useState([]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setFilteredGlossaryData([]);
+
+    const formData = new FormData(e.target);
+    const searchTerm = {};
+
+    for (let [key, value] of formData.entries()) {
+
+      searchTerm[key] = value;
+    } 
+    const value = searchTerm.searchWord.toLowerCase();
+
+    const filteredData = glossaryData.filter( wordObject => {
+      return (
+        wordObject.english.toLowerCase().includes(value) ||
+        wordObject.hungarian.toLowerCase().includes(value)
+      );
+    });
+    console.log(filteredData);
+    setFilteredGlossaryData(filteredData);
+  };
+
   const handleDeleteClick = (id) => {
     setWordToDelete(id);
     setShowDeleteModal(true);
@@ -40,7 +65,7 @@ const GlossaryTable = ({ glossaryData, onDelete, onAdd }) => {
         <label className="block text-gray-700 text-sm font-bold mb-1" htmlFor="searchWord">
             Search word or expression
         </label>
-        <div className='flex'>
+        <form className='flex' onSubmit={(e) => onSubmit(e)}>
           <input
               className="mb-2 shadow appearance-none border rounded w-full py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               name="searchWord"
@@ -48,12 +73,12 @@ const GlossaryTable = ({ glossaryData, onDelete, onAdd }) => {
               type="text"
               disabled={false}
           />
-          <button className='flex items-center justify-center pb-2'>
+          <button type='submit' className='flex items-center justify-center pb-2'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} className="w-7 h-7 stroke-slate-500 hover:stroke-slate-400 ml-2">
               <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
             </svg>
           </button>
-        </div>
+        </form>
 
         <button
           className="mt-6 bg-rose-600 hover:bg-rose-500 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline"
@@ -73,7 +98,7 @@ const GlossaryTable = ({ glossaryData, onDelete, onAdd }) => {
             </tr>
           </thead>
           <tbody>
-            {glossaryData && glossaryData.map((word, index) => (
+            {(filteredGlossaryData.length > 0 ? filteredGlossaryData : glossaryData).map((word, index) => (
               <tr key={index} className="text-xs xl:text-base  hover:bg-slate-100 border-b border-slate-200">
                 <td className="px-2 md:px-4 sm:table-cell">{word.english}</td>
                 <td className="px-2 md:px-4 sm:table-cell">{word.hungarian}</td>
