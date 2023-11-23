c2ce5c53684fa38f88478bc3eba87be1a763d538
mayu0126 <ildikopinter0126@gmail.com>
2023-11-23 01:01:18 +0100

    Update search field design

--- frontend/src/components/GlossaryTable/GlossaryTable.jsx
+++ frontend/src/components/GlossaryTable/GlossaryTable.jsx
@@ -61,27 +61,29 @@ const GlossaryTable = ({ glossaryData, onDelete, onAdd }) => {
     <h2 className="text-center text-gray-700 xl:text-lg text-auto font-semibold mx-auto mt-20 w-full py-10 xs:py-10 sm:py-16 lg:py-20">GLOSSARY OF WORDS AND PHRASES ACCEPTED BY FOOD LAW</h2>
 
     <div className='flex'>
-      <div className="w-1/4 h-44 p-4 text-center justify-center bg-slate-200 rounded-lg ml-28">
-        <label className="block text-gray-700 text-sm font-bold mb-1" htmlFor="searchWord">
-            Search word or expression
-        </label>
-        <form className='flex' onSubmit={(e) => onSubmit(e)}>
-          <input
-              className="mb-2 shadow appearance-none border rounded w-full py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              name="searchWord"
-              id="searchWord"
-              type="text"
-              disabled={false}
-          />
-          <button type='submit' className='flex items-center justify-center pb-2'>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} className="w-7 h-7 stroke-slate-500 hover:stroke-slate-400 ml-2">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-            </svg>
-          </button>
-        </form>
+      <div className="w-1/4 h-44 text-center justify-center ml-28">
+        <div className='bg-slate-200 p-4 rounded-lg'>
+          <label className="block text-gray-700 text-sm font-bold mb-1" htmlFor="searchWord">
+              Search word or expression
+          </label>
+          <form className='flex' onSubmit={(e) => onSubmit(e)}>
+            <input
+                className="mb-2 shadow appearance-none border rounded w-full py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                name="searchWord"
+                id="searchWord"
+                type="text"
+                disabled={false}
+            />
+            <button type='submit' className='flex items-center justify-center pb-2'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} className="w-7 h-7 stroke-slate-500 hover:stroke-slate-400 ml-2">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+              </svg>
+            </button>
+          </form>
+        </div>
 
         <button
-          className="mt-6 bg-rose-600 hover:bg-rose-500 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline"
+          className="mt-12 bg-rose-600 hover:bg-rose-500 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline"
           onClick={handleAddNewWordClick}>
           Add new word or expression
         </button>
